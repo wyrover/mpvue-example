a1203fe9fb3feec0ebd543d8ca49c05c3d1c6b73
wyrover <wyrover@gmail.com>
2018-06-11 12:37:00 +0800

    + init mpvue-example

---
+++ mpvue-example/src/pages/test-page/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    111111111111111111111
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+      logs: []
+    }
+  },
+
+  created () {
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
